b68974a294c5f301f0a37162013370eec12903a4
Mehebub Mukut <mehebub648@gmail.com>
2026-09-03 18:35:55 +0600

    fix request component suggestion export

--- src/lib/requestReasons.ts
+++ src/lib/requestReasons.ts
@@ -2,6 +2,8 @@ export {
   REQUEST_REASON_OPTIONS,
   REQUEST_REASONS,
   matchingRequestReasonOptions,
+  recommendedBloodComponentForReason,
   requestReasonLabel,
+  type RecommendedBloodComponent,
   type RequestReason
 } from '../../server/requestReasons';
